4e591c767487a2bf55bbd0fdb36f2b0d7f822f53
pianista215 <unaisarasola@hotmail.com>
2015-08-17 15:36:21 +0200

    Test

--- src/test/java/com/devsmobile/lesscss/LessCompilerV8Test.java
+++ src/test/java/com/devsmobile/lesscss/LessCompilerV8Test.java
@@ -11,11 +11,9 @@ import com.devsmobile.lesscss.error.LessException;
  */
 public class LessCompilerV8Test {
 
-    /**
-     * Rigourous Test :-)
-     */
+    
     @Test
-    public void testApp()
+    public void testCode()
     {
     	LessCompiler p = new LessCompilerV8();
 
@@ -24,8 +22,24 @@ public class LessCompilerV8Test {
 			@Override
 			public void onLessCompiled(String css, LessException e) {
 				System.out.println(css);
+				assertTrue( true );
 			}
 		});
-        assertTrue( true );
+        
+    }
+    
+    @Test
+    public void testFile()
+    {
+    	LessCompiler p = new LessCompilerV8();
+
+    	p.compileLessFileAsync("test1/test1.less", new LessCallback() {
+			
+			@Override
+			public void onLessCompiled(String css, LessException e) {
+				System.out.println(css);
+				assertTrue( true );
+			}
+		});
     }
 }
